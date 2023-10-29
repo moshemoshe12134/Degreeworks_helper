f68e6bb08f7ed7defe739d68b8a1e2278d139e4d
nitro603 <tavaracristian93@gmail.com>
2023-10-29 13:15:47 -0400

    quick-commit

---
+++ app/src/App.js
@@ -0,0 +1,10 @@
+
+function App() {
+  return (
+    <div className="App">
+      <p>sup dood</p>
+    </div>
+  );
+}
+
+export default App;
---
+++ app/src/components/login.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function login() {
+//import html code here and be able to diplay it
+//after be able to take input and save login info for next screen which will be the main page
+//dont worry about functionality just the basic outline next
+
+  return (
+    <div>login</div>
+  )
+}
+
+export default login
