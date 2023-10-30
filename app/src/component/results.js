1c7f7210765b64ecdffd425bd55afa617c43cd81
nitro603 <tavaracristian93@gmail.com>
2023-10-30 15:51:38 -0400

    changes results on input yipee

---
+++ app/src/component/results.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+function Results({search}) {
+
+    if(search === "CISC. 3140 or 3142 or 3160 or 3171 or 3220")
+        return(
+            <div>1</div>  
+    )
+    if(search === "ECON. 2200 or FINC. 3310 or FINC 3330 or ECON 3400")
+        return(
+            <div>2</div>  
+    )
+    if(search === "BIOL 1002 or BIOL 3003 or BIOL 3004 or CHEM 2200 or CHEM 2201")
+        return(
+            <div>3</div>  
+    )
+ 
+    return(
+        <div></div>
+    )
+
+}
+
+export default Results
--- app/src/pages/main.js
+++ app/src/pages/main.js
@@ -1,7 +1,23 @@
-import React from 'react'
+import {useState,React} from 'react'
 import '../styles.css'
+import Results from '../component/results';
 
-function main() {
+function Main() {
+  const [query, setQuery] = useState("");
+  const [search, setSearch] = useState("")
+
+  //dummy inputs/outputs
+  //Input examples: (following degreeworks format)
+  //Example #1 “CISC. 3140 or 3142 or 3160 or 3171 or 3220”
+  //Example #2 “ECON. 2200 or FINC. 3310 or FINC 3330 or ECON 3400”
+  //Example #3 “BIOL 1002 or BIOL 3003 or BIOL 3004 or CHEM 2200 or CHEM 2201
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // do something with the query
+    setSearch(query);
+  };
+  
 
   return (
     <div class='main'>
@@ -10,12 +26,23 @@ function main() {
           <a href="create_account.html" class="button">Create Account</a>
           <a href="aboutUs.html" class="button">About</a>
       </div>
-      <div class = 'main-square'>
 
+      <div class = 'main-square'>
+        <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Search..."
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
+        />
+        <button type="submit">Search</button>
+        </form>
+        <div class='results'>
+          <Results search={search}></Results>
+        </div>
       </div>
-         
     </div>
   )
 }
 
-export default main
+export default Main
