8a3bb0148caae3e4f5058529f3ef3e9131961f20
nitro603 <tavaracristian93@gmail.com>
2023-10-30 13:18:15 -0400

    commit

--- app/src/pages/main.js
+++ app/src/pages/main.js
@@ -2,8 +2,17 @@ import React from 'react'
 import '../styles.css'
 
 function main() {
+
   return (
     <div class='main'>
+      <div class="top-buttons">
+          <a href="login.html" class="button">Log In</a>
+          <a href="create_account.html" class="button">Create Account</a>
+          <a href="aboutUs.html" class="button">About</a>
+      </div>
+      <div class = 'main-square'>
+
+      </div>
          
     </div>
   )
